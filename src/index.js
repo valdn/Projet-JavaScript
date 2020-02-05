0d63203676376baa44210e1259ca04fb418e79f1
valdn <valentin.di-nardo@etu.univ-amu.fr>
2020-02-05 17:04:43 +0100

    requete

--- src/index.js
+++ src/index.js
@@ -5,8 +5,27 @@ import _ from 'lodash';
     
       // Lodash, currently included via a script, is required for this line to work
       element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    
+      loadData()
       return element;
     }
     
-    document.body.appendChild(component());
+    document.body.appendChild(component())
+
+    function loadData(){
+      const url = 'https://hubeau.eaufrance.fr/api/v0/etat_piscicole/poissons?nom_cours_eau=null&size=20'
+      let oReq = new window.XMLHttpRequest()
+
+      oReq.onload = function(event){
+        let data = JSON.parse(this.responseText)
+        data.data.forEach(element => {
+          console.log(element.nom_poisson);
+        });
+      }
+
+      oReq.onerror = function(event){
+        console.log('Erreur survenue')
+      }
+
+      oReq.open('GET', url)
+      oReq.send(null)
+    }
