2512ddf42959a9de1f25e1fd822b6bb0f736f94e
Arnaud <mouton1.9@hotmail.fr>
2020-02-10 21:57:14 +0100

    changement vers ghibli API

--- src/index.js
+++ src/index.js
@@ -5,45 +5,37 @@ import _ from 'lodash';
       
       // Lodash, currently included via a script, is required for this line to work
       element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-      loadData();
       return element;
     }
     
     document.body.appendChild(component())
 
-    
+    var request = new XMLHttpRequest()
 
-    function loadData(){
-      const url = 'https://hubeau.eaufrance.fr/api/v0/etat_piscicole/poissons?nom_cours_eau=null&size=20'
-      let oReq = new window.XMLHttpRequest()
-
-      oReq.onload = function(event){
-        var poissons = []
-        let data = JSON.parse(this.responseText)
-        data.data.forEach(element => {
-            poissons.push(element.nom_poisson)
-
-        });
-
-        writedata(poissons)
+    request.open('GET', 'https://ghibliapi.herokuapp.com/species', true)
+    request.onload = function() {
+      // Begin accessing JSON data here
+      var data = JSON.parse(this.response)
+      var tableau_espece = []
+      if (request.status >= 200 && request.status < 400) {
+        data.forEach(espece => {
+          tableau_espece.push(espece.name)
+        })
+        writedata(tableau_espece)
+      } else {
+        console.log('error')
       }
-
-      oReq.onerror = function(event){
-        console.log('Erreur survenue')
-      }
-
-      oReq.open('GET', url)
-      oReq.send(null)
-      
     }
 
     function writedata(liste) {
-      const contenu = document.createElement('div');
-      contenu.innerHTML += "<h3> Liste poissons </h3>"
+      const contenu = document.createElement('div')
+      contenu.innerHTML += "<h3> Liste especes </h3>"
       contenu.innerHTML += "<ul>"
       for(var i=0; i < liste.length; i = i+1) {
-        contenu.innerHTML += '<li>' + liste[i] + '</li> </br>'
+        contenu.innerHTML += '<li><a href=\"#\">' + liste[i] + ' </a> </li>'
       }
       contenu.innerHTML += "</ul>"
       document.body.appendChild(contenu)
     }
+    
+    request.send()
