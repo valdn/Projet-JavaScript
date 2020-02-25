22ccf8088a390c69a915361ad6506ac4ec977ab6
Arnaud <mouton1.9@hotmail.fr>
2020-02-25 14:31:28 +0100

    comman-terre

--- src/script/writenames.js
+++ src/script/writenames.js
@@ -7,15 +7,15 @@ import { getImg } from "./getImg.js"
       let c = 0;
       document.getElementById('div_names').hidden=false;
       let contenu = document.getElementById('tabn')
-      contenu.innerHTML=""
-      for(let i=0; i < liste.length; i = i+1) {
-        if (i%3==0){
+      contenu.innerHTML="" // On efface le tableau
+      for(let i=0; i < liste.length; i = i+1) { // ajout des personnages dans le tableau
+        if (i%3==0){ // 3 personnages maximum par ligne
           row = contenu.insertRow(c)
           c++
         }
         let cell = row.insertCell(i%3)
         try{
-          await getImg(liste[i], cell)
+          await getImg(liste[i], cell) // Await permet d'attendre un resolve/reject venant de "promise", cela permet d'éviter l'execution des requetes trop rapidement ce qui entrainerai un blocage venant de l'API
         } catch(error) {
           console.log("Error" , error)
         }
--- src/script/writespecies.js
+++ src/script/writespecies.js
@@ -1,10 +1,13 @@
 import { updated } from "./updated.js"
 
     export function writespecies(liste) {
+
+      // Creation de l'entête de la page pour la selection de l'espèce
       let contenu = document.createElement('div')
-      contenu.id="data"
+      contenu.id="data" // id pour appliquer du CSS
       contenu.innerHTML += "<h3> Liste espèces </h3>"
 
+      // Creation du sélecteur
       let selector = document.createElement('select')
       selector.name="espece"
       selector.id="select_espece"
@@ -13,20 +16,23 @@ import { updated } from "./updated.js"
 
       contenu.appendChild(selector)
       document.body.appendChild(contenu)
+
+      // ajout des options dans le sélecteur en fonction du retour de la requête sur les especes
       let opt = document.createElement('option')
       opt.text = "Choose ..."
-
       document.getElementById('select_espece').options.add(opt)
-      for(let i=0; i < liste.length; i = i+1) {
+      for(let i=0; i < liste.length; i = i+1) { // pour chaque element de la liste on ajoute l'élement dans le selecteur
         let opt = document.createElement('option')
         opt.text = liste[i]
         document.getElementById('select_espece').options.add(opt)
       }
 
+      // Ajout du div qui va contenir la liste des personnages 
       let names = document.createElement('div')
       names.id='div_names'
       names.innerHTML += '<h3 id="lpers"> Liste personnages </h3>'
 
+      // Creation du tableau qui va contenir les personnages 
       let tabn = document.createElement('table')
       tabn.id = 'tabn'
       names.appendChild(tabn)
